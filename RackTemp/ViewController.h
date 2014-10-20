ecf285d9088d992f8e5c8c2cf6e693c308c82fbd
Daniel Wittberger <daniel@mg-mediaservices.com>
2014-10-20 22:30:55 +0200

    Initial Commit

---
+++ RackTemp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RackTemp
+//
+//  Created by Daniel Wittberger on 20.10.14.
+//  Copyright (c) 2014 Wittberger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
